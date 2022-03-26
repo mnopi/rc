f29f15b2a82e6d5f94b31f8f3acae62e2884780b
root <root@example>
2022-03-26 08:29:46 +0100

    auto

--- backup/lib/is-completions.sh
+++ backup/lib/is-completions.sh
@@ -7,3 +7,17 @@ fi
 # autoload -U +X bashcompinit && bashcompinit
 # https://computingforgeeks.com/how-to-source-bash-completion-script-file-in-zsh/
 # https://stackoverflow.com/questions/67136714/how-to-properly-call-compinit-and-bashcompinit-in-zsh
+if [ "x${BASH_VERSION-}" != x -a "x${PS1-}" != x -a "x${BASH_COMPLETION_VERSINFO-}" = x ]; then
+
+    # Check for recent enough version of bash.
+    if [ "${BASH_VERSINFO[0]}" -gt 4 ] ||
+        [ "${BASH_VERSINFO[0]}" -eq 4 -a "${BASH_VERSINFO[1]}" -ge 2 ]; then
+        [ -r "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion" ] &&
+            . "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion"
+        if shopt -q progcomp && [ -r /usr/share/bash-completion/bash_completion ]; then
+            # Source completion code.
+            . /usr/share/bash-completion/bash_completion
+        fi
+    fi
+
+fi
