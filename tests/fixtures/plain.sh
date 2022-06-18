02b63fdbcd4e05edaa556ce71e9ad0e827532619
root <root@example.com>
2022-06-18 18:54:28 +0200

    moving to bbin shell tests

--- tests/fixtures/plain.sh
+++
@@ -1,7 +0,0 @@
-cd "$(dirname "$0")"/../.. || exit
-
-if [ "${1-}" ]; then
-  "$1" -c ". ./lib/shell.sh && echo \${SH}"
-else
-  . ./lib/shell.sh && printf '%s' "${SH}"
-fi
--- tests/fixtures/shebang.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"/../.. || exit
-
-if [ "${1-}" ]; then
-  "$1" -c ". ./lib/shell.sh && echo \${SH}"
-else
-  . ./lib/shell.sh && printf '%s' "${SH}"
-fi
